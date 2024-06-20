aa42c6b81dece2fc8f95cd39bbaeb286e867cb56
Gabriel Souza <gabrielsouzades@gmail.com>
2024-06-20 15:54:45 -0300

    adequacao model motorista

--- src/model/Motorista.js
+++ src/model/Motorista.js
@@ -4,6 +4,8 @@ const motoristaSchema = new mongoose.Schema(
     {
         id: {type: String},
         nome: {type: String, required: true},
+        email: {type: String, required: true},
+        telefone: {type: String, required: true},
         numeroCNH: {type: String, required: true}
     }
 )
