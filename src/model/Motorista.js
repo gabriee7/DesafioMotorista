8400c94908b275a282f3cd6061c9c8599bf21d1e
Gabriel Souza <gabrielsouzades@gmail.com>
2024-06-20 18:07:47 -0300

    mudanca model campos numericos

--- src/model/Motorista.js
+++ src/model/Motorista.js
@@ -5,8 +5,8 @@ const motoristaSchema = new mongoose.Schema(
         id: {type: String},
         nome: {type: String, required: true},
         email: {type: String, required: true},
-        telefone: {type: String, required: true},
-        numeroCNH: {type: String, required: true}
+        telefone: {type: Number, required: true},
+        numeroCNH: {type: Number, required: true}
     }
 )
 
